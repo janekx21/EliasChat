f79057f7b50129989b8b06426e06bbb2bdc99530
janekx21 <janekx21@gmail.com>
2017-04-09 19:03:48 +0200

    update script

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function myFunction(){
+	var url = "janekx21.lima-city.de/projects/webProjects/project11/chat.php";
 	//alert(feld.value);
 	//document.location = "http://google.de/search?q="+feld.value;
-	demo.innerHTML += userName.value + ": " + feld.value + "<br>";
+	demo.innerHTML = check(url+"?user=" + userName.value + "&msg=" + feld.value,"",write);
 	feld.value = "";
+}
 
+function write(txt){
+	demo.innerHTML = txt;
 }
 
 function start(){
@@ -15,3 +19,17 @@ function start(){
 window.onload = function(){
 	start();
 };
+
+
+function check (url,data,callback){
+   var request = new XMLHttpRequest();
+   request.open("GET",url);
+   request.addEventListener('load', function(event) {
+      if (request.status >= 200 && request.status < 300) {
+         callback(request.responseText);
+      } else {
+         console.warn(request.statusText, request.responseText);
+      }
+   });
+   request.send(data);
+}
